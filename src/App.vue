0a2f2fc022beb9d8b6f7faa6fc4aa1455eb02973
sanath24 <sanathkamath24@gmail.com>
2022-06-09 21:57:19 -0400

    base project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <nav class="flex justify-center bg-blue-300 w-full p-5 border-blue-400 border-b-2">
+      <h1 class="text-4xl font-bold tracking-widest">KIRDLE</h1>
+    </nav>
+
+  </div>
+ 
+</template>
+
