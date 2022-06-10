5633a6fd4c9ea8d9c3d5fe8ea29df5ca6cbab8b9
sanath24 <sanathkamath24@gmail.com>
2022-06-10 12:01:27 -0400

    board ui complete

---
+++ src/components/Tile.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="box-border bg-white select-none border w-full inline-flex justify-center items-center text-2xl font-bold leading-8 align-middle w-16 h-16">
+    A
+</div>
+</template>
